7b3e703bd875197def6950a5689ac057bfc50470
brewchetta <chett.tiller@gmail.com>
2023-04-17 11:35:16 -0400

    setup for alembic

--- lib/models.py
+++ lib/models.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 
 # we don't need to import everything from sqlalchemy below but we can import all of these things...
-from sqlalchemy import (create_engine, Column, DateTime, Integer, String)
+from sqlalchemy import (create_engine, Column, Integer, String)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 class Student(Base):
-    __tablename__ = '???'
+    __tablename__ = 'students'
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
 
-if __name__ == '__main__':
-    engine = create_engine('sqlite:///:memory:')
-    Base.metadata.create_all(engine)
+    def __repr__(self):
+        return f"<Student id={self.id} name={self.name}>"
